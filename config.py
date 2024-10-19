4be3730f14ce855b8f71ab6ef2b3aaa1542d660e
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-10-20 03:06:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class Config:
     API_ID = "28243586"
     API_HASH = "4022d5686b9b7a7cf8891205921a0ab3"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    TOKEN = "7489087428:AAHRuMjgt5LiE9QLUsfUm-2JG16x1r0CKec"
     TOKEN = os.environ.get("TOKEN", None)
     MONGO_URL = "mongodb+srv://anmol0700:anmol0700@cluster0.x6zba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
-    SUDOERS = filters.user(["5340652544"])
+    SUDOERS = filters.user(["6929340267"])
